b20c9a6e58b808aa005d9deaad1baa112bdf570d
Vagner Lima Almeida <vagneralmeida72@gmail.com>
2026-08-25 16:05:34 -0400

    Adiciona projeto inicial Delphi

---
+++ SistemaDelphi/SistemaClientes.pas
@@ -0,0 +1,25 @@
+unit SistemaClientes;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TfrmSistemaClientes = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+
+  end;
+
+var
+  frmSistemaClientes: TfrmSistemaClientes;
+
+implementation
+
+{$R *.dfm}
+
+end.
